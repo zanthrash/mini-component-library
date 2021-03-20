08a3ff57c4302dfca8c03d873b700614f3cc235c
zanthrash <zanthrash@gmail.com>
2021-03-20 17:21:15 -0500

    finisnh progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,11 +1,72 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+    radius: 4,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    radius: 4,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    radius: 8,
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = STYLES[size];
+  if (!styles) {
+    throw new Error(`Unknown size ${size} submitted`);
+  }
+  return (
+    <Wrapper
+      value={value}
+      max="100"
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        "--padding": styles.padding + "px",
+        "--radius": styles.radius + "px",
+      }}
+    >
+      <VisuallyHidden>{value} %</VisuallyHidden>
+      <BarWrapper>
+        <Bar
+          style={{ "--width": value + "%", "--height": styles.height + "px" }}
+        />
+      </BarWrapper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: var(--radius);
+  padding: var(--padding);
+`;
+
+const BarWrapper = styled.div`
+  overflow: hidden;
+  border-radius: 4px;
+`;
+
+const Bar = styled.div`
+  height: var(--height);
+  width: var(--width);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+`;
 
 export default ProgressBar;
