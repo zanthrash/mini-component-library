036ba65e1988af9956fd624045334f4d12612ee2
zanthrash <zanthrash@gmail.com>
2021-03-21 09:08:08 -0500

    icon input completed

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,87 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const STYLES = {
+  small: {
+    iconSize: 16,
+    height: 24,
+    borderThickness: 1,
+    fontSize: 14,
+  },
+  large: {
+    iconSize: 24,
+    height: 36,
+    fontSize: 18,
+    borderThickness: 2,
+  },
 };
+
+const IconInput = ({ label, icon, width = 250, size, ...delegated }) => {
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(`Styles missing for size: ${size}`);
+  }
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ "--size": styles.iconSize + "px" }}>
+        <Icon id={icon} strokeWidth={1} size={styles.iconSize} />
+      </IconWrapper>
+      <TextInput
+        {...delegated}
+        style={{
+          "--width": width + "px",
+          "--height": styles.height / 16 + "rem",
+          "--fontSize": styles.fontSize / 16 + "rem",
+          "--borderThickness": styles.borderThickness + "px",
+        }}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.label`
+  position: relative;
+  display: block;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  height: var(--size);
+  width: var(--size);
+`;
+
+const TextInput = styled.input`
+  height: var(--height);
+  width: var(--width);
+  font-size: var(--fontSize);
+  border: none;
+  border-bottom: var(--borderThickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
 
 export default IconInput;
